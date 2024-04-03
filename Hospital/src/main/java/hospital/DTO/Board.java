6c650213fecedda5c246735020efa62e79fa27de
DoheeKim <ehgml7184@naver.com>
2024-04-03 20:09:11 +0900

    Board - DAO, DTO, Service 완료

--- Hospital/src/main/java/hospital/DTO/Board.java
+++ Hospital/src/main/java/hospital/DTO/Board.java
@@ -4,12 +4,92 @@ import java.util.Date;
 
 public class Board {
 
-	int no;
-	String user_id;
-	String category;
-	String title;
-	String content;
-	Date reg_date;
-	Date upd_date;
+	
+	/** (DTO)
+	 *  게시글 정보
+	 *  - 게시글 번호
+	 *  - 유저 아이디(작성자)
+	 *  - 카테고리
+	 *  - 제목
+	 *  - 내용
+	 *  - 등록일자
+	 *  - 수정일자
+	 */
+	
+	private int no;
+	private String user_id;
+	private String category;
+	private String title;
+	private String content;
+	private Date reg_date;
+	private Date upd_date;
+	
+	
+	// 생성자
+	public Board() {
+		this("제목없음", "글쓴이없음", "내용없음");
+	}
+	
+	public Board(String title, String user_id, String content) {
+		this.title = title;
+		this.user_id = user_id;
+		this.content = content;
+		this.reg_date = new Date();
+		this.upd_date = new Date();
+	}
+	
+	
+	// getter,setter...
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(Date reg_date) {
+		this.reg_date = reg_date;
+	}
+	public Date getUpd_date() {
+		return upd_date;
+	}
+	public void setUpd_date(Date upd_date) {
+		this.upd_date = upd_date;
+	}
+
+	// toString...
+	@Override
+	public String toString() {
+		return "Board [no=" + no + ", user_id=" + user_id + ", category=" + category + ", title=" + title + ", content="
+				+ content + ", reg_date=" + reg_date + ", upd_date=" + upd_date + "]";
+	}
+	
+	
 	
 }
